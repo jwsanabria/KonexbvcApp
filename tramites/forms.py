935b29c72db2c527a634e2c56b0e04c6ed1b2cf8
John_Sanabria <jwsanabria@gmail.com>
2020-09-27 15:51:56 -0500

    ultimo cambio

--- tramites/forms.py
+++ tramites/forms.py
@@ -9,21 +9,18 @@ class TramiteForm(forms.ModelForm):
             'nombre',
             'correo',
             'tramite',
-            'comunicacion',
             'solicitud',
         ]
         labels = {
             'nombre': 'Nombres',
             'correo': 'Correo',
             'tramite': 'Trámite',
-            'comunicacion': 'Preferencia Comunicación',
             'solicitud': 'Solicitud',
         }
         widgets = {
             'nombre': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ingrese su nombre'}),
             'correo': forms.EmailInput(attrs={'class':'form-control', 'aria-describedby':'emailHelp', 'placeholder':'Ingrese su email'}),
             'tramite': forms.Select(attrs={'class':'form-control', 'placeholder':'Tipo de trámite'}),
-            'comunicacion': forms.Select(attrs={'class':'form-control', 'placeholder':'Medio de respuesta'}),
             'solicitud': forms.Textarea(attrs={'class':'form-control','placeholder':'Ingrese su solicitud', 'rows':10}),
         }
 
